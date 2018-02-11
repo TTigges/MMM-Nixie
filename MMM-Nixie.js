9e958ce9b35be960ab3372c6b85d5d9cddc818af
Torben Tigges <torben-tigges@web.de>
2018-02-12 00:15:46 +0100

    initial commit

---
+++ MMM-Nixie.js
@@ -0,0 +1,140 @@
+/* global Module */
+
+/* Magic Mirror
+ * Module: MMM-Nixie
+ *
+ * By Torben Tigges
+ * MIT Licensed.
+ */
+
+Module.register("MMM-Nixie",{
+
+	// Default module config.
+	defaults: {
+		tubes: true,
+		wire: "on", // "on", off", "fine", "heavy"
+		inactive: "on", // "on", off", "dimm"
+		dimension: false
+//		size: "medium" // "small", "medium", "big", "huge"
+	},
+
+	getStyles: function() {
+		return [
+			this.file("nixie.css")
+		]
+	},
+
+	getScripts: function() {
+		return [
+			'https://code.jquery.com/jquery-2.2.3.min.js',
+		]
+	},
+
+	start: function() {
+		Log.info("Starting module: " + this.name);
+		var self = this;
+		setInterval(function() {
+			self.nixieClock();
+		}, 1000);
+	},
+
+	getDom: function() {
+		var tubes = ["hh","h","mm","m","ss","s"];
+
+		var nixie = document.createElement("div");
+		nixie.setAttribute("id", "nixie");
+		if (this.config.dimension) {
+			nixie.classList.add("dimension");
+		}
+		if (this.config.inactive === "on"){
+			nixie.classList.add("in");
+		}
+		else if (this.config.inactive === "off"){
+			nixie.classList.add("inoff");
+		}
+		else if (this.config.inactive === "dimm"){
+			nixie.classList.add("indimm");
+		}
+
+		if (this.config.size === "small") {
+			nixie.classList.add("small");
+		}
+		else if (this.config.size === "medium") {
+			nixie.classList.add("medium");
+		}
+		else if (this.config.size === "big") {
+			nixie.classList.add("big");
+		}
+		else if (this.config.size === "huge") {
+			nixie.classList.add("huge");
+		}
+
+		for (t = 0; t < tubes.length; t++) {
+			var newtube = document.createElement("div");
+			newtube.setAttribute("id", tubes[t]);
+			newtube.classList.add("digit");
+			if (this.config.tubes) {
+				newtube.classList.add("tube");
+			}
+			else {
+				newtube.classList.add("notube");
+			}
+
+			for (d = 0; d < 10; d++) {
+				var newdigit = document.createElement("span");
+				newdigit.setAttribute("id", tubes[t] + d);
+				newdigit.classList.add(tubes[t]);
+				newdigit.classList.add("d" + d);
+				newdigit.classList.add("layer");
+				newdigit.classList.add("inactive");
+				newdigit.innerHTML = d;
+				newtube.appendChild(newdigit);
+			}
+			if (this.config.wire === "on" || this.config.wire === "fine" || this.config.wire === "heavy") {
+				if (this.config.wire === "fine") {
+					var wires = 1;
+				}
+				else if (this.config.wire === "heavy") {
+					var wires = 3;
+				}
+				else {
+					var wires = 2;
+				}
+				for (w = 0; w < wires; w++) {
+					var wire = document.createElement("span");
+					wire.classList.add("wire");
+					wire.classList.add("wire" + w);
+					newtube.appendChild(wire);
+				}
+			}
+			if (this.config.tubes) {
+				for (g = 0; g < 4; g++) {
+					var glass = document.createElement("span");
+					glass.classList.add("glass");
+					glass.classList.add("glass" + g);
+					newtube.appendChild(glass);
+				}
+			}
+			nixie.appendChild(newtube);
+		}
+		return nixie;
+	},
+
+	nixieClock: function() {
+		var now = new Date();
+	    for (c = 0; c <= 9; c++) {
+	    		$(".layer").removeClass("active").addClass("inactive");
+	    }
+	    var h = ("0" + now.getHours()).slice(-2);
+	    var m = ("0" + now.getMinutes()).slice(-2);
+	    var s = ("0" + now.getSeconds()).slice(-2);
+	//  console.log(h[0] + h[1] + ":" + m[0] + m[1] + ":" + s[0] + s[1]);
+	    $("#hh"+h[0]).removeClass("inactive").addClass("active");
+	    $("#h"+h[1]).removeClass("inactive").addClass("active");
+	    $("#mm"+m[0]).removeClass("inactive").addClass("active");
+	    $("#m"+m[1]).removeClass("inactive").addClass("active");
+	    $("#ss"+s[0]).removeClass("inactive").addClass("active");
+	    $("#s"+s[1]).removeClass("inactive").addClass("active");
+	}
+
+});
---
+++ Readme.md
@@ -0,0 +1,53 @@
+# MMM-Nixie
+![Preview Screenshot, MMM-Nixie](https://abload.de/img/mmm-nixiencqie.jpg)
+=====================
+Who doesn't like Nixie tubes? This module is bringing a Nixie tube clock to your MagicMirror. It's not just another clock, it has all the layers for each digit (0 to 9), cycling through them, lighting up the active layer/digit – just like a real nixie tube.
+## Installation
+Navigate into your MagicMirror's `modules` folder and execute `git clone https://github.com/TTigges/MMM-Nixie`.
+## Using the module
+Add the module to the modules array in the `config/config.js` file:
+````javascript
+modules: [
+	{
+		module: 'MMM-Nixie',
+		position: 'bottom',
+		config: {
+			tubes: true,
+			wire: "on", // "on", off", "fine", "heavy"
+			inactive: "on", // "on", off", "dimm"
+			dimension: false
+		}
+	}
+
+]
+````
+## Configuration options
+The following properties can be configured:
+| Option                       | Description
+| ---------------------------- | -----------
+| `tubes`                      | **Default:** `true` <br><br> When set to `false`, the fake glass tubes are not shown.
+| `wire`                       | The glowing digits in a nixie tube are surrounded by a fine wire cage. This sets the visibility of the wire mesh.<br><br>**Possible values:** `"on"`, `"off"`, `"fine"`, `"heavy"`<br><br>**Note:** Automatically deactivated (set to `0`) when `tubes: false`
+| `inactive`                   | A nixie tube has 10 digits (0 - 9) and only the active digits glow up. In case of the last digit, when active, 9 inactive digits can be seen in front of it.<br><br>**Default:** `on`<br><br>Can be set to `off` or `dimm`
+| `dimension`                  | To show the layers of digits, `dimension` can be set to `true` to fake the depth of the nixie tube.<br><br>**Default:** `false`
+## Developer Notes
+This is a fun gimmick with no real function. I'm considering using a switch that hides all modules except this clock during night time.
+Some things I'd like to add to the configuration: sizing (small, medium, big, huge), color choice, ...
+
+The MIT License (MIT)
+=====================
+
+Copyright © 2018 TTigges
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the “Software”), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+**The software is provided “as is”, without warranty of any kind, express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement. In no event shall the authors or copyright holders be liable for any claim, damages or other liability, whether in an action of contract, tort or otherwise, arising from, out of or in connection with the software or the use or other dealings in the software.**
